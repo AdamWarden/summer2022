c1968262fa64d58bdf68723e71cf2bb03483e0df
awarden07 <awarden07@gmail.com>
2022-07-13 09:07:43 -0400

    updated programs 1-5

--- COMP1020/Dailys/program1.c
+++ COMP1020/Dailys/program1.c
@@ -27,10 +27,16 @@ char* reverse(char* word)
 
    while(word_begin < word_end)
    {
-    word[word_begin] ^= word[word_end]; //takes the value of word_begin and XOR's it with word_end
-    word[word_end] ^= word[word_begin]; //XOR's word_end so now word_begin = word_end
+    word[word_begin] ^= word[word_end]; //takes the value of word_begin and XOR's it with word_end, stores result to word_begin
+    word[word_end] ^= word[word_begin]; //takes the value of word_end and XOR's it with word_begin, sotres result in word_end
     word[word_begin] ^= word[word_end]; //XOR's word_begin again so now word_end = word_begin and the values are swapped
-    //printf("%s\n", word);
+    /*
+    a=1. b=2
+    *a = 0001, *b = 0010
+    *a = 0001 ^ 0010 = 0011
+    *b = 0011 ^ 0010 = 0001
+    *a = 0011 ^ 0001 = 0010
+    */
 
     word_begin++; // move on to next value from the start in array
     word_end--; //moves to the next value from the end in array

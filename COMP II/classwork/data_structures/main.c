439dd09551383b4f6179c6a36f6802847445a93d
anonn07anonn <53498894+anonn07anonn@users.noreply.github.com>
2022-07-14 17:28:56 -0400

    updated stack code

--- COMP II/classwork/data_structures/main.c
+++ COMP II/classwork/data_structures/main.c
@@ -4,17 +4,16 @@
 
 int main(int argc, char* argv[])
 {
-    STACK hStack;
+	STACK hStack;
 
-    hStack = stack_init_default();
-    if(hStack == NULL)
-    {
-        printf("Failed to allocate space for stack object\n");
-        exit(1);
-    }
+	hStack = stack_init_default();
+	if (hStack == NULL)
+	{
+		printf("Failed to allocate space for stack object.\n");
+		exit(1);
+	}
 
-
-    stack_destroy(&hStack);
-
-    return 0;
+	stack_destroy(&hStack);
+	
+	return 0;
 }
--- COMP II/classwork/data_structures/status.h
+++ COMP II/classwork/data_structures/status.h
@@ -1,10 +1,10 @@
 #ifndef STATUS_H
 #define STATUS_H
 
-enum status {FAILURE, SUCCESS};
-typedef status Status;
+enum status { FAILURE, SUCCESS };
+typedef enum status Status;
 
-enum boolean {FALSE, TRUE};
-typedef boolean Boolean;
+enum boolean { FALSE, TRUE };
+typedef enum boolean Boolean; 
 
 #endif

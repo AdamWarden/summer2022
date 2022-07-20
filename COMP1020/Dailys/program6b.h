44cbec7d1cc101dc370043ef70d72fb6d905f98f
anonn07anonn <53498894+anonn07anonn@users.noreply.github.com>
2022-07-20 03:41:04 +0000

    program6b

--- COMP1020/Dailys/program6b.h
+++ COMP1020/Dailys/program6b.h
@@ -1,9 +1,27 @@
-void reverse_list(Node** head_ref);
+/*
+struct node;
+typedef struct node Node;
+
+struct node
+{
+    Node* next;
+    int* data;
+};
+*/
+
+typedef struct Node {
+    int data;
+    struct Node* next;
+} Node;
+
+Node* reverse_list(Node* head_ref);
+
+Node* newNode(int data);
 
 void head_insert(Node** head_ref, int new_data);
 
-Node* list_sum(Node* head1, Node head2);
+Node* list_sum(Node* head_ref, Node* head_ref2);
 
 void print_list(Node* node);
 
-//void destroy_list()
+//void destroy_list(Node** head_ref);

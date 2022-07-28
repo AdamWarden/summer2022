db9b15125066d3585946a4feb463bfa2ee0b7395
awarden07 <awarden07@gmail.com>
2022-07-28 14:54:25 +0000

    generic_vector updated an json files added

--- COMPII/classwork/generic_vector/generic_vector.h
+++ COMPII/classwork/generic_vector/generic_vector.h
@@ -1,18 +1,27 @@
 #ifndef GENERIC_VECTOR_H
 #define GENERIC_VECTOR_H
-#include "status.h"
 #include "generic.h"
+#include "status.h"
 
 typedef void* GENERIC_VECTOR;
 
-GENERIC_VECTOR generic_vector_init_default(Status (*assignment)(ITEM* pLeft, ITEM pRight), void (*destroy)(ITEM* phInt));
-Boolean generic_vector_is_empty(GENERIC_VECTOR hVector);
-Status generic_vector_push_back(GENERIC_VECTOR hVector, ITEM pItem);
+GENERIC_VECTOR generic_vector_init_default(
+	Status(*item_assignment)(ITEM* phLeft, ITEM hRight),
+	void (*item_destroy)(ITEM* phItem)
+	);
+
+Status generic_vector_push_back(GENERIC_VECTOR hVector, ITEM hItem);
+
 Status generic_vector_pop_back(GENERIC_VECTOR hVector);
-int generic_vector_size(GENERIC_VECTOR hVector);
-int generic_vector_capacity(GENERIC_VECTOR hVector);
 
 ITEM* generic_vector_at(GENERIC_VECTOR hVector, int index);
 
-generic_vector_destroy(GENERIC_VECTOR* phVector);
+Boolean generic_vector_is_empty(GENERIC_VECTOR hVector);
+
+
+int generic_vector_get_size(GENERIC_VECTOR hVector);
+int generic_vector_get_capacity(GENERIC_VECTOR hVector);
+
+void generic_vector_destroy(GENERIC_VECTOR* phVector);
+
 #endif
--- COMPII/classwork/generic_vector/main.c
+++ COMPII/classwork/generic_vector/main.c
@@ -1,56 +1,91 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "status.h"
 #include "generic_vector.h"
 
-Status integer_assignment(ITEM* pLeft, ITEM pRight);
-void integer_destroy(ITEM* phInt);
+Status integer_assignment(ITEM* phLeft, ITEM hRight);
+void integer_destroy(ITEM* phItem);
 
-int main(int argc, char *argv[])
+int main(int argc, char* argv[])
 {
-    Status (*assignment)(ITEM* pLeft, ITEM pRight);
-    void (*destroy)(ITEM* phInt);
+	GENERIC_VECTOR hVector;
+	int number;
+	int i;
 
-    GENERIC_VECTOR hVector;
+	hVector = generic_vector_init_default(integer_assignment, integer_destroy);
 
-    int x = 42;
-    int y = 54;
-    int* p = NULL;
+	if (hVector == NULL)
+	{
+		printf("Failed to allocate space for generic vector object.\n");
+		exit(1);
+	}
+	number = 7;
+	if (!generic_vector_push_back(hVector, &number))
+	{
+		printf("Failed to push_back 7\n");
+	}
+	else
+	{
+		printf("Successfully pushed back 7 into the generic vector.\n");
+	}
+	number = 8;
+	if (!generic_vector_push_back(hVector, &number))
+	{
+		printf("Failed to push_back 8\n");
+	}
+	else
+	{
+		printf("Successfully pushed back 8 into the generic vector.\n");
+	}
 
-    integer_assignment(&p, &x);
-    printf("the final value of *p is %d\n", *p);
+	printf("%d/%d\n", generic_vector_get_size(hVector), generic_vector_get_capacity(hVector));
 
-    hVector = generic_vector_init_default(integer_assignment, integer_destroy);
-    if(hVector == NULL)
-    {
-        printf("Failed to allocate space for vector\n");
-        exit(1);
-    }
 
-    generic_vector_destroy(&hVector);
-    return 0;
+	for (i = 0; i < generic_vector_get_size(hVector); i++)
+	{
+		printf("%d\n", *((int*)(*generic_vector_at(hVector, i))));
+	}
+
+	generic_vector_pop_back(hVector);
+	printf("%d/%d\n", generic_vector_get_size(hVector), generic_vector_get_capacity(hVector));
+
+	generic_vector_pop_back(hVector);
+	printf("%d/%d\n", generic_vector_get_size(hVector), generic_vector_get_capacity(hVector));
+
+	if (generic_vector_pop_back(hVector))
+	{
+		printf("Why did this work?\n");
+	}
+	else
+	{
+		printf("Failed as expected...\n");
+	}
+	generic_vector_destroy(&hVector);
+	
+	return 0;
 }
 
-Status integer_assignment(ITEM* pLeft, ITEM pRight)
+Status integer_assignment(ITEM* phLeft, ITEM hRight)
 {
-    if(*pLeft == NULL)
-    {
-        *pLeft = (int*)malloc(sizeof(int));
-        if(*pLeft == NULL)
-        {
-            printf("Failed to allocate space for integer\n");
-            return _DARWIN_FEATURE_ONLY_UNIX_CONFORMANCE;
-        }
-    }
-    *(int*)(*pLeft) = *((int*)pRight);
-    return SUCCESS;
+	
+	int* pLeft = (int*) *phLeft;
+	int* pRight = (int*) hRight;
+	if (pLeft == NULL)
+	{
+		pLeft = (int*)malloc(sizeof(int));
+		if (pLeft == NULL)
+		{
+			return FAILURE;
+		}
+		*phLeft = pLeft;
+	}
+
+	*pLeft = *pRight;	
+	return SUCCESS;
 }
 
-void integer_destroy(ITEM* phInt)
+void integer_destroy(ITEM* phItem)
 {
-    if(*phInt != NULL)
-    {
-        free(*phInt);
-        *phInt = NULL;
-    }
+	int* pInt = *phItem;
+
+	free(pInt);
 }
